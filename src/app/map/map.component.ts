d841d9ebfade4550e5c10aa4701cb5bff9b9b476
hhoar <hhoareau@gmail.com>
2024-11-08 12:58:14 +0100

    Add dropping screen

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,7 +1,7 @@
 import {AfterViewInit, Component, inject, Input, OnChanges, SimpleChanges} from '@angular/core';
 import * as L from 'leaflet';
 
-import {control, LatLng, LeafletMouseEvent, TileLayer} from 'leaflet';
+import {Circle, CircleMarker, control, LatLng, LeafletMouseEvent, Marker, TileLayer} from 'leaflet';
 import {setParams, showMessage} from '../../tools';
 import {GeolocService} from '../geoloc.service';
 import {environment} from '../../environments/environment';
@@ -31,6 +31,9 @@ export class MapComponent implements OnChanges,AfterViewInit  {
   center: any;
   private infos: string=""
   private layer: TileLayer | undefined
+  private vision: CircleMarker<any> | undefined;
+  private target: Marker<any> | undefined;
+  private me:  Marker<any> | undefined;
 
 
 
@@ -38,9 +41,8 @@ export class MapComponent implements OnChanges,AfterViewInit  {
     try{
       this.user.loc=await this.geolocService.getCurrentPosition()
       this.initializeMap()
-      this.initMarkers()
-      this.setMap()
-      this.add_tokemon_marker()
+      this.center_to_loc()
+      this.add_tokemon_to_markers()
       this.refresh()
     }catch (err:any){
       showMessage(this,'Error getting location: ' + err.message)
@@ -50,27 +52,47 @@ export class MapComponent implements OnChanges,AfterViewInit  {
 
 
   ngOnChanges(changes: any): void {
-    if(!changes.lat.firstChange)this.setMap();
+    if(!changes.lat.firstChange)this.center_to_loc();
   }
 
   private initializeMap() {
     this.map = L.map('map');
-    this.layer=L.tileLayer(baseMapURl).addTo(this.map);
+    this.vision = L.circleMarker([this.map.getCenter().lng,this.map.getCenter().lat], {
+      color: 'red',fillColor: '#f03',
+      fillOpacity: 0.5,radius: 500
+    }).addTo(this.map);
 
-    this.map.on("moveend",(event)=>{
-      this.user.center_map = event.target.getCenter()
-      this.initMarkers()
-      this.add_tokemon_marker()
-      this.refresh()
-    })
+    L.tileLayer(baseMapURl).addTo(this.map);
+
+    var meIcon = L.icon({
+      iconUrl: 'https://tokemon.f80.fr/assets/icons/person_24dp_5F6368.png',
+      iconSize: [38, 38], // size of the icon
+      iconAnchor: [19, 19], // point of the icon which will correspond to marker's location
+      popupAnchor: [-19, -19] // point from which the popup should open relative to the iconAnchor  
+    });
+
+    var targetIcon = L.icon({
+      iconUrl: '"https://tokemon.f80.fr/assets/icons/target.png"',
+      iconSize: [38, 38], // size of the icon
+      iconAnchor: [19, 19], // point of the icon which will correspond to marker's location
+      popupAnchor: [-19, -19] // point from which the popup should open relative to the iconAnchor  
+    });
+
+    this.target=L.marker([this.map.getCenter().lat, this.map.getCenter().lng],{icon:targetIcon, alt:"me"})
+    this.me=L.marker([this.user.loc.coords.latitude, this.user.loc.coords.longitude],{icon:meIcon, alt:"me"})
+
+    this.target.addTo(this.map)
+    this.me.addTo(this.map)
+
+    this.map.on("moveend",(event:L.LeafletEvent)=>this.movemap(event));
   }
+  
 
 
-  setMap(): void {
+  center_to_loc(): void {
     // Fit the map view to the bounds
     if(this.user.loc){
       this.map.setView(new LatLng(this.user.loc?.coords.latitude,this.user.loc?.coords.longitude),17);
-
       // L.tileLayer(baseMapURl, {
       //   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       // }).addTo(this.map).redraw();
@@ -78,7 +100,7 @@ export class MapComponent implements OnChanges,AfterViewInit  {
   }
 
 
-  async add_tokemon_marker() {
+  async add_tokemon_to_markers() {
     let pos=latLonToCartesian(this.user.loc?.coords.latitude,this.user.loc?.coords.longitude,environment.scale_factor)
     let args=["LesBG", pos.x,pos.y,pos.z, 10000]
 
@@ -88,10 +110,11 @@ export class MapComponent implements OnChanges,AfterViewInit  {
     for(let nft of nfts){
       var giftIcon = L.icon({
         iconUrl: 'https://tokemon.f80.fr/assets/icons/pushpin.png',
+        shadowSize: [20, 20],
         iconSize: [38, 38], // size of the icon
         iconAnchor: [19, 36], // point of the icon which will correspond to marker's location
         popupAnchor: [-19, -19] // point from which the popup should open relative to the iconAnchor  
-      });
+      })
 
       let coords=cartesianToPolar(nft.x,nft.y,nft.z,environment.scale_factor)
 
@@ -101,29 +124,7 @@ export class MapComponent implements OnChanges,AfterViewInit  {
 
       this.markers.push(marker)
     }
-
-
-
   }
-
-
-
-  private initMarkers() {
-
-    this.markers=[]
-    var meIcon = L.icon({
-      iconUrl: 'https://tokemon.f80.fr/assets/icons/person_24dp_5F6368.png',
-      iconSize: [38, 38], // size of the icon
-      iconAnchor: [19, 19], // point of the icon which will correspond to marker's location
-      popupAnchor: [-19, -19] // point from which the popup should open relative to the iconAnchor  
-    });
-
-    this.markers.push(
-      L.marker([this.user.loc.coords.latitude, this.user.loc.coords.longitude],{icon:meIcon, alt:"me"}), // Amman
-    )
-
-  }
-
 
 
   async select_nft(event: LeafletMouseEvent) {
@@ -140,4 +141,11 @@ export class MapComponent implements OnChanges,AfterViewInit  {
     this.markers.forEach(marker => marker.addTo(this.map));
     this.layer?.redraw()
   }
+
+  private movemap(event: L.LeafletEvent) {
+      this.user.center_map = event.target.getCenter()
+      this.add_tokemon_to_markers()
+      this.refresh()
+  }
+
 }

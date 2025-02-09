400f82a982c8536978c3ed3f5a4edb2d4a39c1bd
hhoar <hhoareau@gmail.com>
2025-02-09 05:44:43 +0100

    commit pour publication devnet

--- deploy_devnet.bat
+++ deploy_devnet.bat
@@ -1,5 +1,4 @@
 git commit -a -m "commit pour publication devnet"
-del "./src/CNAME"
-copy "./src/CNAME_devnet" "./src/CNAME"
+copy .\src\CNAME_devnet .\src\CNAME
 call ng build --aot --output-hashing=none --source-map=false --optimization=true --configuration devnet
 call gh-pages -d ./dist/tokemonworld --repo https://github.com/f80dev/TokemonWorldDevnet.git -f -t true -b main -m \"update from main\"",
--- deploy_prod.bat
+++ deploy_prod.bat
@@ -1,5 +1,5 @@
 git commit -a -m "commit pour publication production"
-copy ./src/CNAME_prod ./src/CNAME
+copy .\src\CNAME_prod .\src\CNAME
 call npm version patch
 call ng build --aot --output-hashing=none --source-map=false --optimization=true --configuration production
 call gh-pages -d ./dist/tokemonworld --repo https://github.com/f80dev/TokemonWorldProduction.git -f -t true -b main -m \"update from main\"

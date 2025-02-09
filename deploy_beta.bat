b03f10e3c50be16be4c6d6b6ad77df3659f1e023
hhoar <hhoareau@gmail.com>
2025-02-09 05:22:07 +0100

    commit pour publication

--- deploy_beta.bat
+++ deploy_beta.bat
@@ -1,5 +1,5 @@
 git commit -a -m "commit pour publication"
 copy ./src/CNAME_beta ./src/CNAME
-npm version patch
-ng build --aot --output-hashing=none --source-map=true --optimization=false --configuration development
-gh-pages -d ./dist/tokemonworld --repo https://github.com/f80dev/tokenworld.git -f -t true -b gh-pages -m \"update from gh-pages\"
+start npm version patch
+start ng build --aot --output-hashing=none --source-map=true --optimization=false --configuration development
+start gh-pages -d ./dist/tokemonworld --repo https://github.com/f80dev/tokenworld.git -f -t true -b gh-pages -m \"update from gh-pages\"

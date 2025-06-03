184f562bfb19d988624695b9f411c3fa408c25d8
hhoar <hhoareau@gmail.com>
2025-06-03 17:55:30 +0200

    commit pour publication devnet

--- deploy_testnet.bat
+++ deploy_testnet.bat
@@ -1,6 +1,6 @@
 git commit -a -m "commit pour publication devnet"
 copy .\src\CNAME_testnet .\src\CNAME
 copy .\src\index-testnet.html .\src\index.html
-copy .\src\manifest-testnet.webmanifest .\src\manisfest.webmanifest
+copy .\src\manifest-testnet.webmanifest .\src\manifest.webmanifest
 call ng build --aot --output-hashing=all --source-map=true --optimization=false --configuration testnet
 call gh-pages -d ./dist/tokemonworld --repo https://github.com/f80dev/TokemonTestnet.git -f -t true -b main -m \"update from main\"
